9b19195f9a753a883b4e56edaca5d22b670e7b16
ildang100 <last2599@gmail.com>
2021-01-02 02:43:21 +0900

    프로젝트 생성

---
+++ src/main/java/com/sduty/spring_boot_ex05/SpringBootEx05Application.java
@@ -0,0 +1,13 @@
+package com.sduty.spring_boot_ex05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEx05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEx05Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sduty/spring_boot_ex05/SpringBootEx05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sduty.spring_boot_ex05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEx05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
