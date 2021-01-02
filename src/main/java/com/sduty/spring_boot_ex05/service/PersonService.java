ec2c3bc0c2e0cea7782390cfadf56aa15d712a31
ildang100 <last2599@gmail.com>
2021-01-02 23:33:08 +0900

    PersonService 테스트 추가

--- src/main/java/com/sduty/spring_boot_ex05/service/PersonService.java
+++ src/main/java/com/sduty/spring_boot_ex05/service/PersonService.java
@@ -20,10 +20,17 @@ public class PersonService {
     private BlockRepository blockRepository;
 
     public List<Person> getPeopleExcludeBlocks() {
-        List<Person> people = personRepository.findAll();
-        List<Block> blocks = blockRepository.findAll();
+//        List<Person> people = personRepository.findAll();
+//        List<Block> blocks = blockRepository.findAll();
 
-        List<String> blockNames = blocks.stream().map(Block::getName).collect(Collectors.toList());
-        return people.stream().filter(person -> person.getBlock() == null).collect(Collectors.toList());
+//        List<String> blockNames = blocks.stream().map(Block::getName).collect(Collectors.toList());
+//        return people.stream().filter(person -> person.getBlock() == null).collect(Collectors.toList());
+        return personRepository.findByBlockIsNull();
+    }
+
+    public List<Person> getPeopleByName(String name) {
+//        List<Person> people = personRepository.findAll();
+//        return people.stream().filter(person -> person.getName().equals(name)).collect(Collectors.toList());
+        return personRepository.findByName(name);
     }
 }
--- src/test/java/com/sduty/spring_boot_ex05/service/PersonServiceTest.java
+++ src/test/java/com/sduty/spring_boot_ex05/service/PersonServiceTest.java
@@ -2,12 +2,14 @@ package com.sduty.spring_boot_ex05.service;
 
 import com.sduty.spring_boot_ex05.domain.Block;
 import com.sduty.spring_boot_ex05.domain.Person;
+import com.sduty.spring_boot_ex05.domain.dto.Birthday;
 import com.sduty.spring_boot_ex05.repository.BlockRepository;
 import com.sduty.spring_boot_ex05.repository.PersonRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootTest
@@ -33,11 +35,34 @@ public class PersonServiceTest {
         result.forEach(System.out::println);
     }
 
+    @Test
+    void getPeopleByName() {
+        givenPeople();
+
+        List<Person> result = personService.getPeopleByName("martin");
+
+        result.forEach(System.out::println);
+    }
+
+    @Test
+    void findByBirthDayBetween() {
+        givenPerson("martin", 10, "A", LocalDate.of(1991, 8, 15));
+        givenPerson("jhon", 9, "AB", LocalDate.of(1992, 5, 15));
+        givenPerson("david", 9, "B", LocalDate.of(1993, 4, 15));
+        givenPerson("denis", 10, "A", LocalDate.of(1994, 3, 15));
+        givenPerson("sophia", 9, "AB", LocalDate.of(1991, 2, 15));
+        givenPerson("benny", 9, "B", LocalDate.of(1991, 8, 15));
+
+        List<Person> result = personRepository.findByBirthday(8,15);
+
+        result.forEach(System.out::println);
+    }
+
 
     private void givenPeople() {
-        givenPerson("martin", 10, "A");
-        givenPerson("jhon", 9, "AB");
-        givenPerson("david", 9, "B");
+//        givenPerson("martin", 10, "A");
+//        givenPerson("jhon", 9, "AB");
+//        givenPerson("david", 9, "B");
         givenBlockPerson("dennis", 11, "O");
         givenBlockPerson("martin", 10, "A");
     }
@@ -51,12 +76,14 @@ public class PersonServiceTest {
 
     }
 
-    private void givenPerson(String name, int age, String bloodType) {
-        personRepository.save(new Person(name, age, bloodType));
+    private void givenPerson(String name, int age, String bloodType, LocalDate birthday) {
+        Person person = Person.builder().name(name).age(age).bloodType(bloodType).build();
+        person.setBirthday(new Birthday(birthday.getDayOfYear(), birthday.getMonthValue(), birthday.getDayOfMonth()));
+        personRepository.save(person);
     }
 
     private void givenBlockPerson(String name, int age, String bloodType) {
-        Person blockPerson = new Person(name, age, bloodType);
+        Person blockPerson = Person.builder().name(name).age(age).bloodType(bloodType).build();
         blockPerson.setBlock(givenBlock(name));
 
         personRepository.save(blockPerson);
